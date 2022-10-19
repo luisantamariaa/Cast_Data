db557160519b3b8945644219a5d7e068592b26f4
Luis Santamaria <luis.santamaria@outlook.com>
2022-10-19 11:53:50 -0500

    Add files via upload

---
+++ Exersise7.py
@@ -0,0 +1,10 @@
+"""we can cast a data by setting up the """
+x= str(5) #casting or convert data to string or text
+y= int(3.6) #convert to integer
+z= float(5) # convert to float
+
+print (x)
+print (y)
+print (z)
+
+
